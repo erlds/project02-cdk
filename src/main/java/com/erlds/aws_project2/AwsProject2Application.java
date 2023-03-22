fbe755f5a0d720e1f16b7980f797d353518f6828
Evaristo Ramalho <evaristo.ra@hotmail.com>
2023-03-22 00:00:49 -0300

    initial commit

---
+++ src/main/java/com/erlds/aws_project2/AwsProject2Application.java
@@ -0,0 +1,13 @@
+package com.erlds.aws_project2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsProject2Application.class, args);
+	}
+
+}
