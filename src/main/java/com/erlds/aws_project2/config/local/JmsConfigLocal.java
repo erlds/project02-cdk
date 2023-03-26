f90ebc09a19fb4b250a733caa4054ca67bef0375
Evaristo Ramalho <evaristo.ra@hotmail.com>
2023-03-26 11:19:19 -0300

    Adiciona configuracao de SQS no localstack

--- src/main/java/com/erlds/aws_project2/config/local/JmsConfigLocal.java
+++ src/main/java/com/erlds/aws_project2/config/local/JmsConfigLocal.java
@@ -1,8 +1,12 @@
-package com.erlds.aws_project2.config;
+package com.erlds.aws_project2.config.local;
 
 import com.amazon.sqs.javamessaging.ProviderConfiguration;
 import com.amazon.sqs.javamessaging.SQSConnectionFactory;
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClient;
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -16,8 +20,8 @@ import javax.jms.Session;
 
 @Configuration
 @EnableJms
-@Profile("!local")
-public class JmsConfig {
+@Profile("local")
+public class JmsConfigLocal {
 
     @Value("${aws.region}")
     private String awsRegion;
@@ -28,10 +32,12 @@ public class JmsConfig {
     public DefaultJmsListenerContainerFactory jmsListenerContainerFactory() {
         sqsConnectionFactory = new SQSConnectionFactory(
                 new ProviderConfiguration(),
-                AmazonSQSClientBuilder.standard()
-                        .withRegion(awsRegion)
-                        .withCredentials(new DefaultAWSCredentialsProviderChain())
-                        .build());
+                AmazonSQSClient.builder()
+                .withEndpointConfiguration(new AwsClientBuilder
+                        .EndpointConfiguration("http://localhost:4566",
+                        Regions.US_EAST_1.getName()))
+                .withCredentials(new DefaultAWSCredentialsProviderChain())
+                .build());
 
         DefaultJmsListenerContainerFactory factory =
                 new DefaultJmsListenerContainerFactory();
