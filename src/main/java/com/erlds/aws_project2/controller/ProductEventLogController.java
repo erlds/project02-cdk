ecf1fdc4b3e6185cd6792f17141871690ac2b6e9
Evaristo Ramalho <evaristo.ra@hotmail.com>
2023-04-01 11:48:00 -0300

    Cria busca de eventos pelo codigo e tipo do evento

--- src/main/java/com/erlds/aws_project2/controller/ProductEventLogController.java
+++ src/main/java/com/erlds/aws_project2/controller/ProductEventLogController.java
@@ -1,9 +1,16 @@
 package com.erlds.aws_project2.controller;
 
+import com.erlds.aws_project2.model.ProductEventLogDTO;
 import com.erlds.aws_project2.repository.ProductEventLogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @RestController
 @RequestMapping("/api")
@@ -15,4 +22,30 @@ public class ProductEventLogController {
     public ProductEventLogController(ProductEventLogRepository productEventLogRepository) {
         this.productEventLogRepository = productEventLogRepository;
     }
+
+    @GetMapping("/events")
+    public List<ProductEventLogDTO> getAllEvents() {
+        return StreamSupport
+                .stream(productEventLogRepository.findAll().spliterator(),false)
+                .map(ProductEventLogDTO::new)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/events/{code}")
+    public List<ProductEventLogDTO> getAllEvents(@PathVariable String code) {
+        return productEventLogRepository.findAllByPk(code)
+                .stream()
+                .map(ProductEventLogDTO::new)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/events/{code}/{event}")
+    public List<ProductEventLogDTO> getAllEvents(@PathVariable String code,
+                                                 @PathVariable String event) {
+        return productEventLogRepository.findAllByPkAndSkStartsWith(code,event)
+                .stream()
+                .map(ProductEventLogDTO::new)
+                .collect(Collectors.toList());
+    }
+
 }
