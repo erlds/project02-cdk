ba5243902040a1b45573469b8affc92893cbe1f9
Evaristo Ramalho <evaristo.ra@hotmail.com>
2023-04-02 12:59:57 -0300

    Adiciona configuracao para teste local do dynamodb

---
+++ src/main/java/com/erlds/aws_project2/config/local/DynamoDbConfigLocal.java
@@ -0,0 +1,89 @@
+package com.erlds.aws_project2.config.local;
+
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.model.*;
+import com.erlds.aws_project2.repository.ProductEventLogRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@EnableDynamoDBRepositories(basePackageClasses = ProductEventLogRepository.class)
+@Profile("local")
+public class DynamoDbConfigLocal {
+    private static final Logger LOG = LoggerFactory.getLogger(DynamoDbConfigLocal.class);
+
+    private final AmazonDynamoDB amazonDynamoDB;
+
+    public DynamoDbConfigLocal() {
+        this.amazonDynamoDB = AmazonDynamoDBClient.builder()
+                .withEndpointConfiguration(
+                        new AwsClientBuilder.EndpointConfiguration("http://localhost:4566",
+                            Regions.US_EAST_1.getName()))
+                .withCredentials(new DefaultAWSCredentialsProviderChain())
+                .build();
+
+        DynamoDB dynamoDB = new DynamoDB(amazonDynamoDB);
+
+        List<AttributeDefinition> attributeDefinitions = new ArrayList<AttributeDefinition>();
+        attributeDefinitions.add(new AttributeDefinition().withAttributeName("pk")
+                .withAttributeType(ScalarAttributeType.S));
+        attributeDefinitions.add(new AttributeDefinition().withAttributeName("sk")
+                .withAttributeType(ScalarAttributeType.S));
+
+        List<KeySchemaElement> keySchemaElements = new ArrayList<KeySchemaElement>();
+        keySchemaElements.add(new KeySchemaElement().withAttributeName("pk")
+                .withKeyType(KeyType.HASH));
+        keySchemaElements.add(new KeySchemaElement().withAttributeName("sk")
+                .withKeyType(KeyType.RANGE));
+
+        CreateTableRequest request = new CreateTableRequest()
+                .withTableName("product-events")
+                .withKeySchema(keySchemaElements)
+                .withAttributeDefinitions(attributeDefinitions)
+                .withBillingMode(BillingMode.PROVISIONED)
+                .withProvisionedThroughput(new ProvisionedThroughput(1L, 1L));
+
+        Table table = dynamoDB.createTable(request);
+
+        try {
+            table.waitForActive();
+        } catch (InterruptedException e) {
+            LOG.error(e.getMessage());
+        }
+    }
+
+    @Bean
+    @Primary
+    public DynamoDBMapperConfig dynamoDBMapperConfig() {
+        return DynamoDBMapperConfig.DEFAULT;
+    }
+
+    @Bean
+    @Primary
+    public DynamoDBMapper dynamoDBMapper(AmazonDynamoDB amazonDynamoDB,
+                                         DynamoDBMapperConfig config) {
+        return new DynamoDBMapper(amazonDynamoDB,config);
+    }
+
+    @Bean
+    @Primary
+    public AmazonDynamoDB amazonDynamoDB() {
+        return this.amazonDynamoDB;
+    }
+}
